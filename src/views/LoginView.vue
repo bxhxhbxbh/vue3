bc603791960bfc1bf6d501433e736d784d9277cb
bxhxhbxbh <bxhxhbxbh@163.com>
2022-06-28 21:30:27 +0800

    创建项目

---
+++ src/views/LoginView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    登陆页面
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'loginView',
+});
+</script>
